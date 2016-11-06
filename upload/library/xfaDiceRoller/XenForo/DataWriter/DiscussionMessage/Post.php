bc206e97b4a306cffca171d03e35546de4138c94
Xon <tazipper1@hotmail.com>
2016-11-06 17:34:27 +0800

    cleanup

--- upload/library/xfaDiceRoller/XenForo/DataWriter/DiscussionMessage/Post.php
+++ upload/library/xfaDiceRoller/XenForo/DataWriter/DiscussionMessage/Post.php
@@ -5,18 +5,6 @@ class xfaDiceRoller_XenForo_DataWriter_DiscussionMessage_Post extends XFCP_xfaDi
     protected function _messagePostSave()
     {
         parent::_messagePostSave();
-/*
-        if ($this->isUpdate() && $this->isChanged('thread_id'))
-        {
-            $this->_db->query("
-                update xf_thread set dice_count = dice_count + 1 where thread_id = ?
-            ", array($this->get('thread_id')));
-
-            $this->_db->query("
-                update xf_thread set dice_count = GREATEST(dice_count - 1, 0) where thread_id = ?
-            ", array($this->getExisting('thread_id')));
-        }
-*/
         // only one DataWriter is edited when merging posts
         if (xfaDiceRoller_Globals::$mergeTargetId && 
             xfaDiceRoller_Globals::$mergeTargetId == $this->get('post_id') &&
